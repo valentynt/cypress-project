ae4c338f5f813cdc75e12702a1d68077ed224c76
Valentyn Tyrtychko <valentyntyrtychko@gmail.com>
2024-10-02 19:55:47 +0300

    docker run -it -v ${PWD}:/e2e -w /e2e cypress/included:13.13.1

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -23,43 +23,6 @@ describe("Registration Validation Tests", () => {
         "rgb(220, 53, 69)"
       );
     });
-
-    it("should show an error for invalid 'Name'", () => {
-      cy.get("input#signupName").clear().type("!!");
-      cy.get("input#signupLastName").clear();
-      cy.contains("Name is invalid").should("be.visible");
-      cy.get("input#signupName").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error for wrong 'Name' length (less than 2 characters)", () => {
-      cy.get("input#signupName").clear().type("A");
-      cy.get("input#signupLastName").clear();
-      cy.contains("Name has to be from 2 to 20 characters long").should(
-        "be.visible"
-      );
-      cy.get("input#signupName").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error for wrong 'Name' length (more than 20 characters)", () => {
-      cy.get("input#signupName").clear().type("A".repeat(21));
-      cy.get("input#signupLastName").clear();
-      cy.contains("Name has to be from 2 to 20 characters long").should(
-        "be.visible"
-      );
-      cy.get("input#signupName").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
   });
 
   context("Last Name Field Validations", () => {
@@ -73,43 +36,6 @@ describe("Registration Validation Tests", () => {
         "rgb(220, 53, 69)"
       );
     });
-
-    it("should show an error for invalid 'Last name'", () => {
-      cy.get("input#signupLastName").clear().type("!!");
-      cy.get("input#signupName").clear();
-      cy.contains("Last name is invalid").should("be.visible");
-      cy.get("input#signupLastName").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error for wrong 'Last name' length (less than 2 characters)", () => {
-      cy.get("input#signupLastName").clear().type("A");
-      cy.get("input#signupName").clear();
-      cy.contains("Last name has to be from 2 to 20 characters long").should(
-        "be.visible"
-      );
-      cy.get("input#signupLastName").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error for wrong 'Last name' length (more than 20 characters)", () => {
-      cy.get("input#signupLastName").clear().type("A".repeat(21));
-      cy.get("input#signupName").clear();
-      cy.contains("Last name has to be from 2 to 20 characters long").should(
-        "be.visible"
-      );
-      cy.get("input#signupLastName").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
   });
 
   context("Email Field Validations", () => {
@@ -117,17 +43,6 @@ describe("Registration Validation Tests", () => {
       cy.get("input#signupEmail").clear().type("invalidEmail");
       cy.get("input#signupName").clear();
       cy.contains("Email is incorrect").should("be.visible");
-      cy.get("input#signupEmail").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error for empty 'Email' field", () => {
-      cy.get("input#signupEmail").clear();
-      cy.get("input#signupName").clear();
-      cy.contains("Email required").should("be.visible");
       cy.get("input#signupEmail").should(
         "have.css",
         "border-color",
@@ -149,69 +64,6 @@ describe("Registration Validation Tests", () => {
         "rgb(220, 53, 69)"
       );
     });
-
-    it("should show an error for wrong 'Password' length (more than 15 characters)", () => {
-      cy.get("input#signupPassword").clear().type("A1bcdefghijklmnop");
-      cy.get("input#signupName").clear();
-      cy.contains(
-        "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-      ).should("be.visible");
-      cy.get("input#signupPassword").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error if 'Password' does not contain at least one integer", () => {
-      cy.get("input#signupPassword").clear().type("Abcdefgh");
-      cy.get("input#signupName").clear();
-      cy.contains(
-        "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-      ).should("be.visible");
-      cy.get("input#signupPassword").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error if 'Password' does not contain at least one capital letter", () => {
-      cy.get("input#signupPassword").clear().type("1bcdefgh");
-      cy.get("input#signupName").clear();
-      cy.contains(
-        "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-      ).should("be.visible");
-      cy.get("input#signupPassword").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error if 'Password' does not contain at least one small letter", () => {
-      cy.get("input#signupPassword").clear().type("1ABCDEFGH");
-      cy.get("input#signupName").clear();
-      cy.contains(
-        "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-      ).should("be.visible");
-      cy.get("input#signupPassword").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error for empty 'Password' field", () => {
-      cy.get("input#signupPassword").clear();
-      cy.get("input#signupName").clear();
-      cy.contains("Password required").should("be.visible");
-      cy.get("input#signupPassword").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
   });
 
   context("Re-enter Password Field Validations", () => {
@@ -220,17 +72,6 @@ describe("Registration Validation Tests", () => {
       cy.get("input#signupRepeatPassword").clear().type("DifferentPassw1");
       cy.get("input#signupName").clear();
       cy.contains("Passwords do not match").should("be.visible");
-      cy.get("input#signupRepeatPassword").should(
-        "have.css",
-        "border-color",
-        "rgb(220, 53, 69)"
-      );
-    });
-
-    it("should show an error for empty 'Re-enter password' field", () => {
-      cy.get("input#signupRepeatPassword").clear();
-      cy.get("input#signupName").clear();
-      cy.contains("Re-enter password required").should("be.visible");
       cy.get("input#signupRepeatPassword").should(
         "have.css",
         "border-color",
@@ -247,18 +88,6 @@ describe("Registration Validation Tests", () => {
       cy.get("input#signupPassword").clear().type("short");
       cy.get("input#signupRepeatPassword").clear().type("short");
       cy.get("button").contains("Register").should("be.disabled");
-    });
-
-    it("should successfully register a new user with valid data", () => {
-      cy.get("input#signupName").clear().type("TestName");
-      cy.get("input#signupLastName").clear().type("TestLastName");
-      cy.get("input#signupEmail").clear().type(uniqueEmail);
-      cy.get("input#signupPassword").clear().type(loginPassword);
-      cy.get("input#signupRepeatPassword").clear().type(loginPassword);
-      cy.get("button").contains("Register").click();
-      cy.get("button#userNavDropdown")
-        .contains(" My profile ")
-        .should("be.visible");
     });
   });
 });
